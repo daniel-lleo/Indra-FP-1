cce8d5130ca31512ea3f1a7009b029bb17fa5263
Daniel Lleó <daniellleoc@gmail.com>
2025-05-21 11:19:09 +0200

    Finalizadas todas las clases del programa.

---
+++ src/main/csharp/AppSostenibilidad/AppSostenibilidad/Evento.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppSostenibilidad
+{
+    class Evento
+    {
+        // ATRIBUTOS
+        private string nombre;
+        private DateTime fecha;
+        private string ubicacion;
+        private List<Usuario> participantes;
+        private Categoria categoria;
+
+        // CONSTRUCTOR
+        public Evento(string nombre, DateTime fecha, string ubicacion, Categoria categoria)
+        {
+            this.nombre = nombre;
+            this.fecha = fecha;
+            this.ubicacion = ubicacion;
+            this.categoria = categoria;
+        }
+
+        // MÉTODOS
+        public void AddParticipante(Usuario u)
+        {
+            participantes.Add(u);
+        }
+
+        public void CancelarEvento()
+        {
+            nombre = null;
+            ubicacion = null;
+            categoria = null;
+        }
+    }
+}
---
+++ src/main/csharp/AppSostenibilidad/AppSostenibilidad/Organizador.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppSostenibilidad
+{
+    class Organizador : Usuario
+    {
+        // ATRIBUTOS
+        private List<Evento> eventosCreados;
+
+        // CONSTRUCTOR
+        public Organizador(string nombre, string correo) : base(nombre, correo) {  }
+
+        // MÉTODOS
+        public Evento CrearEvento()
+        {
+            Evento e;
+            string nombre, ubicacion;
+            Categoria categoria = new Categoria();
+            DateTime fecha;
+
+            Console.Write("Introduzca el nombre del evento: ");
+            nombre = Console.ReadLine();
+            Console.Write("Introduzca la fecha del evento: ");
+            fecha = DateTime.Parse(Console.ReadLine());
+            Console.Write("Introduzca la ubicación del evento: ");
+            ubicacion = Console.ReadLine();
+            Console.Write("Introduzca la categoría del evento: ");
+            categoria = categoria.SeleccionarCategoria();
+
+            e = new Evento(nombre, fecha, ubicacion, categoria);
+            return e;
+        }
+    }
+}
