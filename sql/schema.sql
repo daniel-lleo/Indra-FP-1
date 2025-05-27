05b6c4b61e45db801ebcc5d8c18d13e1b4f5823c
Daniel Lleó <daniellleoc@gmail.com>
2025-05-27 12:38:28 +0200

    Creada estructura de base de datos sin atributos para las tablas.

---
+++ sql/schema.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS sostenibilidad;
+USE sostenibilidad;
+
+CREATE TABLE usuarios (
+
+);
+
+CREATE TABLE organizadores (
+
+);
+
+CREATE TABLE categorias (
+
+);
+
+CREATE TABLE eventos (
+
+);
+
+CREATE TABLE inscripciones (
+
+);
