4b190353ca0c7c35bec6e6f1eeb93aaffde5d1c3
Daniel Lleó <daniellleoc@gmail.com>
2025-05-29 14:44:05 +0200

    Añadida clase Ubicacion.

---
+++ src/main/csharp/AppSostenibilidad/AppSostenibilidad/Ubicacion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSostenibilidad
+{
+    internal class Ubicacion
+    {
+        // ATRIBUTOS
+        string ciudad, calle;
+
+        // CONSTRUCTOR
+        public Ubicacion(string ciudad, string calle)
+        {
+            this.ciudad = ciudad;
+            this.calle = calle;
+        }
+    }
+}
